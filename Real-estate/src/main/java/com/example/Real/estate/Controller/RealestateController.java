71608a2839a6324c2b8aeb620fce055c1ebdd7ab
gar505gar <grm_505@hotmail.com>
2022-01-23 12:20:45 +0300

    The back end

---
+++ Real-estate/src/main/java/com/example/Real/estate/Controller/RealestateController.java
@@ -0,0 +1,52 @@
+package com.example.Real.estate.Controller;
+
+import com.example.Real.estate.Model.Entities.Realestate;
+import com.example.Real.estate.Model.Entities.Users;
+import com.example.Real.estate.Model.Servicer.RealestateService;
+import com.example.Real.estate.Model.Servicer.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@CrossOrigin(origins = "https://dashboard.heroku.com/apps/garam1herokuapp.com")
+@RestController
+@RequestMapping(path= "realestate")
+public class RealestateController {
+
+
+    private RealestateService realestateService;
+
+    @Autowired
+    public RealestateController(RealestateService realestateService) {
+        this.realestateService = realestateService;
+    }
+
+    @GetMapping
+    public List<Realestate> getRealestates() {
+        return realestateService.getRealestates();
+    }
+
+   @GetMapping(path = "{realestateId}")
+   public Optional<Realestate> getRealstate(@PathVariable(name = "realestateId") Integer realestateId) {
+        return realestateService.getRealestate(realestateId);
+    }
+    @GetMapping(path = "/getRealestateByUseremail/{email}")
+        public List<Realestate> getUser(@PathVariable("email") String email) {
+        return realestateService.getRealestateByemail(email) ;
+        }
+
+    @PostMapping(path="/addn")
+    public void registerNewRealestate(@RequestBody Realestate realestate){
+        realestateService.addNewRealestate(realestate);
+    }
+
+
+
+    @DeleteMapping(path = "delete/{realestateId}")
+    public void deleteRealestate(@PathVariable ("realestateId") Integer realestateId){
+        realestateService.deleteRealestate(realestateId);
+    }
+}
+
+
---
+++ Real-estate/src/main/java/com/example/Real/estate/Controller/UserController.java
@@ -0,0 +1,55 @@
+package com.example.Real.estate.Controller;
+
+import com.example.Real.estate.Model.Entities.Users;
+import com.example.Real.estate.Model.Servicer.UserService;
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@CrossOrigin(origins = "https://dashboard.heroku.com/apps/garam1herokuapp.com")
+@RestController
+@RequestMapping("api/User")
+
+public class UserController<userService>  {
+
+
+    private UserService userService ;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<Users> getUser() {
+        return userService.getUser();
+    }
+
+    @GetMapping(path = "{userId}")
+    public Optional<Users> getUser(@PathVariable(name = "userId") Integer userId) {
+        return userService.getUser(userId);
+    }
+    @PostMapping("add")
+    public void registerNewUser(@RequestBody Users user){
+        userService.addNewUser(user);
+    }
+
+    @GetMapping(path = "/getuserbyemail/{email}")
+    public Optional<Users> getUser(@PathVariable("email") String email) {
+        System.out.println(email);
+        return userService.getUserByemail(email) ;
+    }
+
+    @PostMapping(path= "login")
+    public String login(@RequestBody Users user){
+        return  userService.getCheck(user.getEmail(), user.getPassword());
+    }
+
+    @DeleteMapping(path = "delete/{userId}")
+    public void deleteArtwork(@PathVariable ("userId") Integer userId){
+        userService.deleteUser(userId);
+    }
+}
+
